3c03e2d45ade428f9c2be5acef5d7c3def4b16c2
DuongHao999 <duongtuhao999@gmail.com>
2025-10-29 19:32:40 +0700

    test

--- WinFormsApp1/obj/Release/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Release/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69ba2aadefcde7e34b20bcfaabba7c0456ba63cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
